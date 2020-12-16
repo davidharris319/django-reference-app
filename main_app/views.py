1be828cded7456ce64c0f51819f3da9cd54bdc1e
David Harris <davidharris319@gmail.com>
2020-12-15 22:42:04 -0500

    Added dog model and details page

--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-class Dog:  # Note that parens are optional if not inheriting from another class
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-
-dogs = [
-    Dog('Madison', 'yellow lab', 'young pupper', 3),
-    Dog('Casey', 'black lab', 'old puper', 15),
-    Dog('Raven', 'mixed-breed', 'lazy pupper', 10)
-]
+from .models import Dog
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('<h1> Hello 🐶 </h1>')
+    return render(request, 'home.html')
 
 
 def about(request):
@@ -28,4 +13,10 @@ def about(request):
 
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
+
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', {'dog': dog})
